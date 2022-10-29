aef5ca8e9b529cd6e0bed90ef31f9b2f29e5dd0d
edmccrea <ed-mccrea@hotmail.co.uk>
2022-10-30 00:08:28 +0200

    refactor: use getLastIndex function

--- client/src/lib/PriceTable.svelte
+++ client/src/lib/PriceTable.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+  import { fade } from "svelte/transition";
+
   import type { MappedFuelData } from "../routes/+layout";
+  import type { FuelEntry } from "../routes/+layout";
   import { formatCompanyName } from "$lib/helpers/format-company-name";
 
   export let fuelData: MappedFuelData;
   export let currentFuelType: string;
+
+  const getLastIndex = (fuelDataArray: FuelEntry[]) => {
+    return fuelDataArray[fuelDataArray.length - 1];
+  };
 </script>
 
 <section>
@@ -17,33 +24,39 @@
         <td>Last 7 Days</td>
       </tr>
     </thead>
-    <tbody>
-      {#key currentFuelType}
+    {#key currentFuelType}
+      <tbody>
         {#each Object.keys(fuelData) as company}
           {#if fuelData[company][currentFuelType].length}
             <tr class="company-row">
               <td>{formatCompanyName(company)}</td>
               <td
-                >{fuelData[company][currentFuelType][
-                  fuelData[company][currentFuelType].length - 1
-                ].price}kr</td
+                ><a href={`/${company}`}
+                  >{getLastIndex(fuelData[company][currentFuelType]).price}kr</a
+                ></td
               >
               <td
-                >{fuelData[company][currentFuelType][
-                  fuelData[company][currentFuelType].length - 1
-                ].difference}kr</td
+                class="difference"
+                class:higher={getLastIndex(fuelData[company][currentFuelType])
+                  .difference > 0}
+              >
+                <a href={`/${company}`}
+                  >{getLastIndex(fuelData[company][currentFuelType])
+                    .difference}kr</a
+                ></td
               >
               <td
-                >{fuelData[company][currentFuelType][
-                  fuelData[company][currentFuelType].length - 1
-                ].last_update}</td
+                ><a href={`/${company}`}
+                  >{getLastIndex(fuelData[company][currentFuelType])
+                    .last_update}</a
+                ></td
               >
-              <td class="last-seven">Graph</td>
+              <td class="last-seven"><a href={`/${company}`}>Graph</a></td>
             </tr>
           {/if}
         {/each}
-      {/key}
-    </tbody>
+      </tbody>
+    {/key}
   </table>
 </section>
 
@@ -64,6 +77,14 @@
   td {
     padding: 0.5rem 1rem;
     border-bottom: 1px solid var(--color-light-grey);
+  }
+
+  .difference {
+    color: var(--color-success);
+  }
+
+  .higher {
+    color: var(--color-error);
   }
 
   .last-seven {
