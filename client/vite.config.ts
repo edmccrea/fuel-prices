d9a2dfa345b2cc502fbdde6dd5775c5f9b2377dd
edmccrea <ed-mccrea@hotmail.co.uk>
2022-10-30 21:00:57 +0100

    refactor: remove comments from config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,20 +6,6 @@ const config: UserConfig = {
   ssr: {
     noExternal: ["chart.js/**"],
   },
-  // build: {
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ["chart.js/auto/auto.js"],
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // for externalized deps
-  //       globals: {
-  //         "chart.js/auto/auto.js": "chart.js/auto/auto.js",
-  //       },
-  //     },
-  //   },
-  // },
 };
 
 export default config;
