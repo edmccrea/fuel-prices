da708dfb95d9cc944bd18d8b1a7470dd9bdb11d8
edmccrea <ed-mccrea@hotmail.co.uk>
2022-11-03 19:04:09 +0100

    refactor: update about page

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -1,1 +1,37 @@
-<h1>About some stuff here</h1>
+<section>
+  <h1>About <span>Us</span></h1>
+  <h2>What's the point<span>?</span></h2>
+  <p>
+    The aim of this site is to provide an overview of the fuel prices of the
+    four major providers in Sweden. Here you can see who is consistently
+    cheapest, trends in prices over time and detailed information on each
+    provider.
+  </p>
+
+  <h2>How we collect the data<span>?</span></h2>
+  <p>
+    We take the publicly available data given by each fuel provider. The
+    information isn't necessarily updated daily. An important thing to note is
+    that these prices can and probably differ to the ones you find in your local
+    station by a little bit.
+  </p>
+</section>
+
+<style>
+  section {
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  h1 {
+    font-size: 54px;
+    padding: 2rem 0;
+  }
+
+  span {
+    color: var(--color-secondary);
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+</style>
